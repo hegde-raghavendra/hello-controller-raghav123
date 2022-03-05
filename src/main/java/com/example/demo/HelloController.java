1e7539597ad8eae0cfeefd14962a24babc80e8e0
Raghavendra Hegde <raghavendra.g.hegde@gmail.com>
2022-03-05 11:59:36 -0600

    updated the controller

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,20 +1,33 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("naveen")
 public class HelloController {
     
     @GetMapping("/AutoflowIndex")
-    public String autoflow() {
-        return "hello world from autoflow";
+    public ResponseEntity<String> autoflow() {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl
+                = "http://dummy.restapiexample.com/api/v1/employees";
+
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(fooResourceUrl, String.class);
+
+        return response;
     }
 
-    @GetMapping("/Parm")
-    public String parm() {
-        return "hello world from parm";
+    @GetMapping("/Parm/{name}")
+    public String parm(@PathVariable String name, @RequestParam String id) {
+        return "hello " + name + " id is " + id;
     }
 }
--- src/main/java/com/example/demo/RaghuController.java
+++ src/main/java/com/example/demo/RaghuController.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.Model.Employee;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +24,11 @@ public class RaghuController {
     public String parm() {
         return "hello world from parm raghav";
     }
+
+    @PostMapping("/Parm")
+    public @ResponseBody ResponseEntity<String> parmPost(@RequestBody Employee emp) {
+
+        return new ResponseEntity<String>(emp.toString(), HttpStatus.ACCEPTED);
+    }
+
 }
